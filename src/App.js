ee63e95029ccf547f33c0135486f32ad780518fe
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-29 11:32:13 +0200

    *Add react-router-dom *Add content to app.js *Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-
+import { Switch, Route, Link } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <h1>Hej</h1>
+    <div>
+      <h1>Hej, världen!</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/chat">Chat</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/contact">
+          <p>Contact</p>
+        </Route>
+        <Route path="/chat">
+          <p>Chat</p>
+        </Route>
+        <Route path="/about">
+          <p>About</p>
+        </Route>
+      </Switch>
     </div>
   );
 }
-
 export default App;
