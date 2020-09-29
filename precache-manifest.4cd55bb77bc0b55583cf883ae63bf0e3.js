508b74f896743ac44c9f8b334358942e18a5598d
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-29 09:36:57 +0200

    Updates

---
+++ precache-manifest.4cd55bb77bc0b55583cf883ae63bf0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4453a230c721e95aaecfb11a807270",
+    "url": "/js-lessons-deploy/index.html"
+  },
+  {
+    "revision": "cb28cc533a02e3b483f4",
+    "url": "/js-lessons-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fe8ee4c9bb69616266e",
+    "url": "/js-lessons-deploy/static/js/2.b283872f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-lessons-deploy/static/js/2.b283872f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb28cc533a02e3b483f4",
+    "url": "/js-lessons-deploy/static/js/main.939fd920.chunk.js"
+  },
+  {
+    "revision": "2b3d6f1d43a1cc8534d4",
+    "url": "/js-lessons-deploy/static/js/runtime-main.1e281e79.js"
+  }
+]);
